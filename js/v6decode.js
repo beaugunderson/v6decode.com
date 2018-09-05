1a448bbe9b053c3ff67e57e96905ae5a1493b510
SimonIT <simonit.orig@gmail.com>
2018-09-05 23:25:32 +0200

    change to get ip over php

--- js/v6decode.js
+++ js/v6decode.js
@@ -126,7 +126,7 @@ function formatArin(data) {
 }
 
 function updateAddressFromRemoteAddress(options) {
-    $.get('ip.py', function (d, statusText, xhr) {
+    $.get('ip.php', function (d, statusText, xhr) {
         var addressString = xhr.getResponseHeader('x-address');
 
         if (addressString) {
